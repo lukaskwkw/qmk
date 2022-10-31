9ba9cc231c288567b292e9938aea509922c0baa0
Łukasz Kwiecień <lukaskwkw@gmail.com>
2022-10-31 12:18:23 +0100

    my_lily58_alt_update

--- keyboards/lily58/keymaps/lukaskwkw/keymap.c
+++ keyboards/lily58/keymaps/lukaskwkw/keymap.c
@@ -7,9 +7,9 @@ enum layer_number {
     _ADJUST,
 };
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV, KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_MINS, KC_LSFT, KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E, KC_I, KC_O, RSFT_T(KC_QUOT), KC_LCTL, KC_X, KC_C, KC_D, KC_V, KC_Z, KC_MPRV, KC_MNXT, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, RCTL_T(KC_EQL), MO(2), KC_LGUI, LALT_T(KC_ESC), KC_SPC, KC_ENT, KC_BSPC, RGUI_T(KC_DEL), MO(1)),
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV, KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_MINS, KC_LSFT, KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E, KC_I, KC_O, RSFT_T(KC_QUOT), KC_LCTL, KC_X, KC_C, KC_D, KC_V, KC_Z, KC_MPRV, RGUI_T(KC_MNXT), KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, RCTL_T(KC_EQL), MO(2), LGUI_T(KC_ESC), KC_LALT, KC_SPC, KC_ENT, KC_BSPC, RALT_T(KC_DEL), MO(1)),
                                                               [1] = LAYOUT(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_PSCR, KC_HOME, KC_END, KC_PGUP, KC_PGDN, KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS, KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_MSTP, KC_PIPE, KC_GRV, KC_PLUS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-                                                              [2] = LAYOUT(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP, KC_UP, KC_PGDN, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_NUM, KC_PLUS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, DF(3), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+                                                              [2] = LAYOUT(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP, KC_UP, KC_PGDN, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLEP, KC_PLUS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, DF(3), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
                                                               [3] = LAYOUT(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_TRNS, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MPRV, KC_MNXT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, DF(0), KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC, KC_ENT, KC_TRNS, KC_TRNS, KC_TRNS)};
 
 layer_state_t layer_state_set_user(layer_state_t state) {
@@ -20,15 +20,15 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {              // Encoder on Master side
         if (IS_LAYER_ON(_LOWER)) { //  On _RAISE layer
             if (clockwise) {
-                tap_code(KC_WH_R); // Move wheel right
+                tap_code(KC_MNXT); // Move wheel right
             } else {
-                tap_code(KC_WH_L); // Move wheel left
+                tap_code(KC_MPRV); // Move wheel left
             }
         } else if (IS_LAYER_ON(_RAISE)) { //  On _RAISE layer
             if (clockwise) {
-                tap_code(KC_MS_R); // Move cursor right
+                tap_code(KC_PPLS); // Move cursor right
             } else {
-                tap_code(KC_MS_L); // Move cursor left
+                tap_code(KC_PMNS); // Move cursor left
             }
         } else { // On other layers
             if (clockwise) {
@@ -46,15 +46,15 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             }
         } else if (IS_LAYER_ON(_LOWER)) { //  On _RAISE layer
             if (clockwise) {
-                tap_code(KC_MS_D); // Move cursor down
+                tap_code(KC_WH_D); // Move cursor down
             } else {
-                tap_code(KC_MS_U); // Move cursor up
+                tap_code(KC_WH_U); // Move cursor up
             }
         } else { // On other layers
             if (clockwise) {
-                tap_code(KC_WH_D); // Move wheel down
+                tap_code(KC_PGDN); // Move wheel down
             } else {
-                tap_code(KC_WH_U); // Move wheel up
+                tap_code(KC_PGUP); // Move wheel up
             }
         }
     }
